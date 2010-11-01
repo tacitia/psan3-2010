b4ea94d3f00cbee0801641953361b88ed73bb6cb
lillian.g621 <lillian.g621@2ef5a7e3-2128-a117-80c9-53a425c450cf>
2010-11-01 06:07:18 +0000

    Testing SCM

---
+++ trunk/Prototypes/CocoaMacServer/TestingSCM.h
@@ -0,0 +1,16 @@
+//
+//  TestingSCM.h
+//  CocoaMacServer
+//
+//  Created by Guo Hua on 01/11/2010.
+//  Copyright 2010 HKUST. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface TestingSCM : NSObject {
+
+}
+
+@end
