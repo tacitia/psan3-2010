be14bb20f3da1725e2ba7ca4e84a75575ce2ff4f
lillian.g621 <lillian.g621@2ef5a7e3-2128-a117-80c9-53a425c450cf>
2010-11-01 07:06:33 +0000

    Testing SCM

---
+++ prototypes/CocoaMacServer/Server.h
@@ -0,0 +1,16 @@
+//
+//  Server.h
+//  CocoaMacServer
+//
+//  Created by Guo Hua on 01/11/2010.
+//  Copyright 2010 HKUST. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+
+@interface Server : NSObject {
+
+}
+
+@end
