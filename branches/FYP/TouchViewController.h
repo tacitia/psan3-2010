c4beb4c1f5d16a55c897fdbfa72bb96350859b3a
karidashell <karidashell@2ef5a7e3-2128-a117-80c9-53a425c450cf>
2011-02-11 10:39:12 +0000

    recognize long press by printing "long press"

---
+++ branches/FYP/TouchViewController.h
@@ -0,0 +1,38 @@
+//
+//  TouchViewController.h
+//  RDPPrototype
+//
+//  Created by Karida on 18/01/2011.
+//  Copyright 2011 HKUST. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <UIKit/UIKit.h>
+
+@interface TouchViewController : UIViewController <UIScrollViewDelegate> {
+	
+	//UITapGestureRecognizer *tapRecognizer;
+	//UISwipeGestureRecognizer *swipeLeftRecognizer;
+	//UILongPressGestureRecognizer * longPressRecognizer;
+	//IBOutlet UIScrollView *imageScrollView;
+	
+	UIImage * image;
+	UIImageView *imageView;
+}
+
+//@property (nonatomic, retain) UITapGestureRecognizer *tapRecognizer;
+//@property (nonatomic, retain) UISwipeGestureRecognizer *swipeLeftRecognizer;
+//@property (nonatomic, retain) UILongPressGestureRecognizer *longPressRecognizer;
+//@property (nonatomic, retain) IBOutlet UIScrollView *imageScrollView;
+
+@property (nonatomic, retain) UIImageView *imageView;
+@property (nonatomic, retain) UIImage *image;
+
+/*
+ @interface MultiTouchViewController : UIImageView {
+ 
+ }*/
+
+
+
+@end
