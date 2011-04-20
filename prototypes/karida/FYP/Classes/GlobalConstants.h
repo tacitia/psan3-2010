33a166e2007f4d5f6fd862d47d95662fa2e431c0
lillian.g621 <lillian.g621@2ef5a7e3-2128-a117-80c9-53a425c450cf>
2011-04-20 13:48:27 +0000

    globalconstants

---
+++ prototypes/karida/FYP/Classes/GlobalConstants.h
@@ -0,0 +1,37 @@
+//
+//  GlobalConstants.h
+//  RDPPrototype
+//
+//  Created by Guo Hua on 20/04/2011.
+//  Copyright 2011 HKUST. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+#pragma mark Virtual Key Codes
+
+extern const int VK_CANCEL = 0x03; //Control-breaking process
+extern const int VK_BACK = 0x08;
+extern const int VK_TAB = 0x09;
+extern const int VK_RETURN = 0x0D; //Enter
+extern const int VK_SHIFT = 0x10;
+extern const int VK_CONTROL = 0x11;
+extern const int VK_MENU = 0x12; //ALT key
+extern const int VK_CAPITAL = 0x14;
+extern const int VK_ESCAPE = 0x1B;
+extern const int VK_SPACE = 0x20;
+extern const int VK_PRIOR = 0x21; //Page up
+extern const int VK_NEXT = 0x22; //Page down
+extern const int VK_END = 0x23;
+extern const int VK_HOME = 0x24;
+extern const int VK_LEFT = 0x25;
+extern const int VK_UP = 0x26;
+extern const int VK_RIGHT = 0x27;
+extern const int VK_DOWN = 0x28;
+extern const int VK_SNAPSHOT = 0x2C;
+extern const int VK_INSERT = 0x2D;
+extern const int VK_DELETE = 0x2E;
+extern const int VK_F1 = 70;
+
+ 
---
+++ prototypes/karida/FYP/Classes/GlobalConstants.m
@@ -0,0 +1,13 @@
+//
+//  GlobalConstants.m
+//  RDPPrototype
+//
+//  Created by Guo Hua on 20/04/2011.
+//  Copyright 2011 HKUST. All rights reserved.
+//
+
+
+
+@implementation GlobalConstants
+
+@end
